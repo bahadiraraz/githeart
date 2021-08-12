6dbfe3ec9668d5073fc146d5c7944cf48c7439e1
BAHADIRking <b123qweasdzxc@gmail.com>
2021-08-12 16:06:22 +0300

    1.0.3
    print_color and clear_color functions added

--- githeart/interface.py
+++ githeart/interface.py
@@ -2,153 +2,156 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QAbstractButton
 from PyQt5.QtGui import QColor, QPainter, QPen
 import keyboard
-
+import numpy as np
 
 current_color = 0
 flag = True
+
+
 class GithubTabloItem(QAbstractButton):
-    """
-    Github Tablo tarzı Widget'ın elemanı
+	"""
+	Github Tablo tarzı Widget'ın elemanı
 
-    clicked sinyali başka bir şey için kullanılabilir,
-    renk değiştirmek için mouseReleaseEvent kullanılıyor
-    """
+	clicked sinyali başka bir şey için kullanılabilir,
+	renk değiştirmek için mouseReleaseEvent kullanılıyor
+	"""
 
-    renkler = (
-        QColor(22, 27, 34),
-        QColor(155, 233, 168),
-        QColor(64, 196, 99),
-        QColor(48, 161, 78),
-        QColor(33, 110, 57),
-    )
-    
-    def __init__(self):
-        super().__init__()
-        self.setMinimumSize(11,11)
-        self.setStyleSheet("border: 0px;")
-        self.setStyleSheet("border-color: black;")
-        self.setCursor(Qt.PointingHandCursor)
-        self.renk = 0
+	renkler = (
+		QColor(22, 27, 34),
+		QColor(155, 233, 168),
+		QColor(64, 196, 99),
+		QColor(48, 161, 78),
+		QColor(33, 110, 57),
+	)
 
-    def mouseReleaseEvent(self, event):
-        self.renk += 1
-        if self.renk > len(self.renkler) - 1:
-            self.renk = 0
+	def __init__(self):
+		super().__init__()
+		self.setMinimumSize(11, 11)
+		self.setStyleSheet("border: 0px;")
+		self.setStyleSheet("border-color: black;")
+		self.setCursor(Qt.PointingHandCursor)
+		self.renk = 0
 
-        self.update()
+	def mouseReleaseEvent(self, event):
+		self.renk += 1
+		if self.renk > len(self.renkler) - 1:
+			self.renk = 0
 
-    def paintEvent(self, event):
-        painter = QPainter()
-        painter.begin(self)
-     
-        painter.setPen(QPen(Qt.black, 1))
-        painter.setBrush(self.renkler[self.renk])
-        painter.drawRect(0, 0, self.width()-1, self.height()-1)
+		self.update()
 
-        painter.end()
+	def paintEvent(self, event):
+		painter = QPainter()
+		painter.begin(self)
 
-    def keyPressEvent(self, event):
-        global current_color
-        global flag
-        if event.key() == Qt.Key_1:
-            print(current_color)
-            current_color = 0
-            flag = True
-        elif event.key() == Qt.Key_2:
-            print(current_color)
-            current_color = 1
-            flag = True
-        elif event.key() == Qt.Key_3:
-            print(current_color)
-            current_color = 2
-            flag = True
-        elif event.key() == Qt.Key_4:
-            print(current_color)
-            current_color = 3
-            flag = True
-        elif event.key() == Qt.Key_5:
-            print(current_color)
-            current_color = 4
-            flag = True
-        elif event.key() == Qt.Key_Escape:
-            flag = False
-            print("escape")
-            self.renk = 0
-            self.update()
-  
-            
-        self.update()
-    
-    def mousePressEvent(self, event):
-        global current_color
-        current_color = (self.renk +1) % 4
-        self.update()
-	
-    #if press the r key block enterEvent
-    
+		painter.setPen(QPen(Qt.black, 1))
+		painter.setBrush(self.renkler[self.renk])
+		painter.drawRect(0, 0, self.width() - 1, self.height() - 1)
 
+		painter.end()
 
+	def keyPressEvent(self, event):
+		global current_color
+		global flag
+		if event.key() == Qt.Key_1:
 
-    def enterEvent(self, event):
-        global flag
-        if flag:
-            global current_color
-            print(current_color)
-            self.renk = current_color
+			current_color = 0
+			flag = True
+		elif event.key() == Qt.Key_2:
 
+			current_color = 1
+			flag = True
+		elif event.key() == Qt.Key_3:
 
+			current_color = 2
+			flag = True
+		elif event.key() == Qt.Key_4:
 
-    
-        
-    
+			current_color = 3
+			flag = True
+		elif event.key() == Qt.Key_5:
+
+			current_color = 4
+			flag = True
+		elif event.key() == Qt.Key_Escape:
+			flag = False
+			print("escape")
+
+		self.update()
+
+	def mousePressEvent(self, event):
+		global current_color
+		current_color = (self.renk + 1) % 4
+		self.update()
+
+	def enterEvent(self, event):
+		global flag
+		if flag:
+			global current_color
+			self.renk = current_color
+
 
 class GithubTablo(QWidget):
-    """
-    Github Tablo tarzı Widget
+	"""
+	Github Tablo tarzı Widget
 
-    parent  ->  Parent widget
-    cols    ->  Sütun sayısı (int)
-    rows    ->  Satır sayısı (int)
-    """
-    def __init__(self, parent, cols, rows):
-        super().__init__(parent)
+	parent  ->  Parent widget
+	cols    ->  Sütun sayısı (int)
+	rows    ->  Satır sayısı (int)
+	"""
 
-        self.layout = QGridLayout()
-        self.setLayout(self.layout)
-        self.layout.setSpacing(3)
-        for y in range(rows):
-            for x in range(cols):
-                self.layout.addWidget(GithubTabloItem(), x, y, alignment=Qt.AlignCenter)
+	def __init__(self, parent, cols, rows):
+		super().__init__(parent)
 
+		self.layout = QGridLayout()
+		self.setLayout(self.layout)
+		self.layout.setSpacing(3)
+		for y in range(rows):
+			for x in range(cols):
+				self.layout.addWidget(GithubTabloItem(), x, y, alignment=Qt.AlignCenter)
 
 
 class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-   
+		self.layout = QVBoxLayout()
+		self.setLayout(self.layout)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+		self.githubtablo = GithubTablo(self, 7, 50)
+		self.layout.addWidget(self.githubtablo)
 
-        self.githubtablo = GithubTablo(self, 7, 50)
-        self.layout.addWidget(self.githubtablo)
+		p = self.palette()
+		p.setColor(self.backgroundRole(), QColor(13, 17, 23))
+		self.setPalette(p)
 
+		self.setWindowTitle("Github Tablo")
 
-        p = self.palette()
-        p.setColor(self.backgroundRole(), QColor(13, 17, 23))
-        self.setPalette(p)
-        
-        self.setWindowTitle("Github Tablo")
+		def print_colors():
+			g = list()
+			for y in range(50):
+				for x in range(7):
+					g.append(self.githubtablo.layout.itemAtPosition(x, y).widget().renk)
+			g = np.matrix(g).reshape(50, 7)
+			g = np.rot90(g, 3)
+			g = np.fliplr(g)
+			g = g.tolist()
+			for i in g: print(i)
+			print()
 
+		def clear_colors():
+			for y in range(50):
+				for x in range(7):
+					self.githubtablo.layout.itemAtPosition(x, y).widget().renk = 0
+					self.githubtablo.layout.itemAtPosition(x, y).widget().update()
+
+		keyboard.add_hotkey('p', print_colors)
+		keyboard.add_hotkey('c', clear_colors)
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+	app = QApplication([])
 
-    mw = MainWindow()
-    mw.show()
+	mw = MainWindow()
+	mw.show()
 
-    app.exec_()
-
-
+	app.exec_()
